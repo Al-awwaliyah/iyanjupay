cc53f9ac3daf0690afdcfc3fc5c9f6f7c3f552ef
Aremu Lawal <lawalaremu53@gmail.com>
2026-08-18 15:57:38 +0100

    Remove AppSplash component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppSplash from "./components/AppSplash";
 import ResetPassword from "./pages/ResetPassword";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -15,8 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-
-      <AppSplash>
+      
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
@@ -24,7 +22,6 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </AppSplash>
 
     </TooltipProvider>
   </QueryClientProvider>
